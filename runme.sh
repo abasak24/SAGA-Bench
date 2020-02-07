4177d4d33a055f651397c2725a0ef989e95b7a44
abasak24 <abanti@dhcp185-155.ece.ucsb.edu>
2020-02-06 21:08:02 -0800

    changed runme.sh

--- runme.sh
+++ runme.sh
@@ -15,5 +15,8 @@
 # Run Wiki-Talk
 #./frontEnd -d 1 -w 0 -f ${dataDir}wiki-talk-pure.shuffle.t.w.csv -b 500000 -s adListChunked -n 2394385 -a bfsdyn -t 64
 
-#Run test.csv on algorithm "traverse". "Traverse" is a single-threaded micro-kernel that reads the neighbors of all vertices in the graph
+#Run test.csv 
 ./frontEnd -d 1 -w 1 -f ./test.csv -b 10 -t 24 -s stinger -n 40 -a prdyn
+
+#Run test.csv on algorithm "traverse". "Traverse" is a single-threaded micro-kernel that reads the neighbors of all vertices in the graph
+#./frontEnd -d 1 -w 1 -f ./test.csv -b 10 -t 24 -s stinger -n 40 -a traverse
